d79fcf65978145d1b9d7cd327076318cfe4fd4b7
harpreetsingh <harpreets@nevada.unr.edu>
2017-11-09 18:25:36 -0800

    load users

--- twitter-client/LoadDatasetsFromCSV.py
+++ twitter-client/LoadDatasetsFromCSV.py
@@ -1,4 +1,54 @@
+import os
+import psycopg2 as psyco
+import csv
+import glob
+import json
+from datetime import datetime
+from numpy import long
+import SqlStatements
+import pprint
+
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+PROPERTIES_FILE = 'twitter-client.properties'
+properties = dict(line.strip().split('=') for line in open(ROOT_DIR + '/' + PROPERTIES_FILE))
+
+conn = psyco.connect(dbname="cs_776", user="system", password="system", host="localhost")
 
 
-# load users (authentic and bots)
-# load tweets
+def loadUsers(usersDir):
+    if not usersDir:
+        print("usersDir is not provided.")
+        return
+
+    for file_name in glob.glob1(ROOT_DIR + '/' + usersDir, "*"):
+        genuineUsers = False
+        if 'genuine' in file_name:
+            genuineUsers = True
+
+        usersDataFile = ROOT_DIR + '/' + usersDir + file_name
+        with conn:
+            with conn.cursor() as curs:
+                with open(usersDataFile) as inFile:
+                    userReader = csv.DictReader(inFile)
+                    for line in userReader:
+                        row = json.loads(json.dumps(line).replace("\\ufeff", ""))
+                        row['is_user_genuine'] = genuineUsers
+
+                        pprint.pprint(row)
+                        insertUser(conn, curs, SqlStatements.mapInputToUsers(row))
+
+    return
+
+
+def insertUser(conn, cursor, data):
+    try:
+        cursor.execute(SqlStatements.INSERT_INTO_USERS, data)
+    except Exception as e:
+        print("""Exception occurred during user insert, continuing loop.\ndata: {}\nException: {}""".format(data, e))
+        conn.rollback()
+
+    return
+
+
+loadUsers(properties.get("loadDataSet.users-directory"))
---
+++ twitter-client/SqlStatements.py
@@ -0,0 +1,106 @@
+from numpy import long
+from datetime import datetime
+
+# noinspection SqlNoDataSourceInspection,SqlDialectInspection
+INSERT_INTO_USERS = """INSERT INTO tss_dev.users 
+                          (user_id, name, screen_name,
+                           statuses_count, followers_count,
+                           friends_count, favourites_count,
+                           listed_count, url, lang, time_zone,
+                           location, default_profile, default_profile_image,
+                           geo_enabled, profile_image_url, profile_banner_url,
+                           profile_use_background_image, profile_background_image_url_https,
+                           profile_text_color, profile_image_url_https,
+                           profile_sidebar_border_color, profile_background_tile,
+                           profile_sidebar_fill_color, profile_background_image_url,
+                           profile_background_color, profile_link_color, utc_offset,
+                           is_translator, follow_request_sent, protected,
+                           verified, notifications, description, contributors_enabled,
+                           following, created_at, timestamp, crawled_at, updated, is_user_genuine)
+VALUES (%(user_id)s, %(name)s, %(screen_name)s, 
+        %(statuses_count)s, %(followers_count)s, 
+        %(friends_count)s, %(favourites_count)s, 
+        %(listed_count)s, %(url)s, %(lang)s, %(time_zone)s, 
+        %(location)s, %(default_profile)s, %(default_profile_image)s, 
+        %(geo_enabled)s, %(profile_image_url)s, %(profile_banner_url)s, 
+        %(profile_use_background_image)s, %(profile_background_image_url_https)s, 
+        %(profile_text_color)s, %(profile_image_url_https)s, 
+        %(profile_sidebar_border_color)s, %(profile_background_tile)s, 
+        %(profile_sidebar_fill_color)s, %(profile_background_image_url)s, 
+        %(profile_background_color)s, %(profile_link_color)s, %(utc_offset)s, 
+        %(is_translator)s, %(follow_request_sent)s, %(protected)s, 
+        %(verified)s, %(notifications)s, %(description)s, %(contributors_enabled)s, 
+        %(following)s, %(created_at)s, %(timestamp)s, %(crawled_at)s, %(updated)s, %(is_user_genuine)s)"""
+
+
+def mapInputToUsers(inputData):
+    return {'user_id': convertInputToLong(inputData['id']),
+            'name': stringNullCheck(inputData['name']),
+            'screen_name': stringNullCheck(inputData['screen_name']),
+            'statuses_count': convertInputToLong(inputData['statuses_count']),
+            'followers_count': convertInputToLong(inputData['followers_count']),
+            'friends_count': convertInputToLong(inputData['friends_count']),
+            'favourites_count': convertInputToLong(inputData['favourites_count']),
+            'listed_count': convertInputToLong(inputData['listed_count']),
+            'url': stringNullCheck(inputData['url']),
+            'lang': stringNullCheck(inputData['lang']),
+            'time_zone': stringNullCheck(inputData['time_zone']),
+            'location': stringNullCheck(inputData['location']),
+            'default_profile': convertInputToBool(inputData['default_profile']),
+            'default_profile_image': convertInputToBool(inputData['default_profile_image']),
+            'geo_enabled': convertInputToBool(inputData['geo_enabled']),
+            'profile_image_url': stringNullCheck(inputData['profile_image_url']),
+            'profile_banner_url': stringNullCheck(inputData['profile_banner_url']),
+            'profile_use_background_image': convertInputToBool(inputData['profile_use_background_image']),
+            'profile_background_image_url_https': stringNullCheck(inputData['profile_background_image_url_https']),
+            'profile_text_color': stringNullCheck(inputData['profile_text_color']),
+            'profile_image_url_https': stringNullCheck(inputData['profile_image_url_https']),
+            'profile_sidebar_border_color': stringNullCheck(inputData['profile_sidebar_border_color']),
+            'profile_background_tile': convertInputToBool(inputData['profile_background_tile']),
+            'profile_sidebar_fill_color': stringNullCheck(inputData['profile_sidebar_fill_color']),
+            'profile_background_image_url': stringNullCheck(inputData['profile_background_image_url']),
+            'profile_background_color': stringNullCheck(inputData['profile_background_color']),
+            'profile_link_color': stringNullCheck(inputData['profile_link_color']),
+            'utc_offset': convertInputToLong(inputData['utc_offset']),
+            'is_translator': convertInputToBool(inputData['is_translator']),
+            'follow_request_sent': stringNullCheck(inputData['follow_request_sent']),
+            'protected': convertInputToBool(inputData['protected']),
+            'verified': convertInputToBool(inputData['verified']),
+            'notifications': stringNullCheck(inputData['notifications']),
+            'description': stringNullCheck(inputData['description']),
+            'contributors_enabled': convertInputToBool(inputData['contributors_enabled']),
+            'following': stringNullCheck(inputData['following']),
+            'created_at': stringNullCheck(inputData['created_at']),
+            'timestamp': convertInputToTimestamp(inputData['created_at'], '%a %b %d %H:%M:%S +0000 %Y'),
+            'crawled_at': convertInputToTimestamp(inputData['crawled_at'], '%m/%d/%y %H:%M'),
+            'updated': convertInputToTimestamp(inputData['updated'], '%m/%d/%y %H:%M'),
+            'is_user_genuine': inputData['is_user_genuine']
+            }
+
+
+def stringNullCheck(input):
+    if not input:
+        return None
+
+    return input.strip()
+
+
+def convertInputToBool(input):
+    if not input:
+        return False
+
+    return True if input.strip() == '1' else False
+
+
+def convertInputToTimestamp(input, fmt):
+    if not input:
+        return None
+
+    return datetime.strptime(input.strip(), fmt)
+
+
+def convertInputToLong(input):
+    if not input:
+        return None
+
+    return long(input.strip())
